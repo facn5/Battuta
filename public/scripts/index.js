dc786605ea6f3a6260c1338b08b72504a1fc6441
misterrodger <johndavidrodger@gmail.com>
2019-04-18 11:14:51 +0300

    Fix bugs in POST method for adding rides
    -Also fixed some bugs in fetch for get all rides
    -Removed unneeded driver.html page
    Relates #34

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,45 +1,22 @@
-"use strict";
-
-const bcrypt = require('bcryptjs');
-
-//accepts a string, async returns a string
-const hashPassword = (password, callback) => {
-  bcrypt.genSalt(10, (err, salt) => {
-    if(err) {
-      callback(err);
-    } else {
-      bcrypt.hash(password, salt, callback);
-    }
-  });
-};
-
-//accepts 2 strings, async returns a Boolean
-const comparePasswords = (password, hashedPassword, callback) => {
-  bcrypt.compare(password, hashedPassword, callback);
-};
-
-function getRides(callback) {
-  fetch('/getRides')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    return callback(data);
-  })
-  .catch(function(error) {
-    return (error);
-  })
-}
-
-(function () {
-// getRides();
-
-
-// let form = document.querySelector('form');
-// form.addEventListener("submit", event => {
-//   event.preventDefault();
-//   console.log("adding ride:", form.elements.value);
-// });
-}) ();
-
-module.exports = {comparePasswords, hashPassword};
+// function getRides(callback) {
+//   fetch('/getRides')
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     return callback(data);
+//   })
+//   .catch(function(error) {
+//     return (error);
+//   })
+// }
+//
+// (function () {
+// // getRides();
+//
+// // let form = document.querySelector('form');
+// // form.addEventListener("submit", event => {
+// //   event.preventDefault();
+// //   console.log("adding ride:", form.elements.value);
+// // });
+// }) ();
--- src/queries/postData.js
+++ src/queries/postData.js
@@ -3,15 +3,13 @@ const getData = require('./getData');
 
 const addRide = (driverid, pickup, dropoff, price, response, cb) => {
   dbConnection.query(
-    // `SELECT * FROM rides;`,
     `INSERT INTO rides (driver_id, pickup, dropoff, price) VALUES ($1, $2, $3, $4);`,
     [driverid, pickup, dropoff, price],
     (err, res) => {
       console.log(res);
-      // console.log(err)
-      // console.log(res.rows)
+      console.log(res.rows)
       if (err) return cb(err);
-      cb(null, res, console.log(response));
+      cb(null, res);
     }
   )
 }
